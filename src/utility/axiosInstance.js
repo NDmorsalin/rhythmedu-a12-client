4a9a569d187d2911e90ec660d877770958f3de68
ND Morsalin <morsalin056@gmail.com>
2023-06-12 22:53:03 +0600

    remote url of backend  added

--- src/utility/axiosInstance.js
+++ src/utility/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://rhythmedu-a12.vercel.app/api',
   withCredentials: true,
 });
 
