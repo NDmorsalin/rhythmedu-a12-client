f2fb7ea4f1c2d465856e2197ba6cf8c5edcd4785
ND Morsalin <morsalin056@gmail.com>
2023-06-22 09:03:06 +0600

    change local url to remote url

--- src/utility/axiosInstance.js
+++ src/utility/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://rhythmedu-a12.vercel.app/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://rhythmedu-a12.vercel.app/api',
+  // baseURL: 'http://localhost:5000/api',
   withCredentials: true,
 });
 
